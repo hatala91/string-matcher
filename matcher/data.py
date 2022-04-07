372098463b5cdfee9a28d5b66dc41b7e0b18672b
Sebastian Hätälä <19345882+hatala91@users.noreply.github.com>
2022-04-07 16:23:18 +0200

    Apply autoformat

--- matcher/data.py
+++ matcher/data.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
+
 from azureml.core import Dataset, Run, Workspace
+
 
 def _get_workspace() -> Workspace:
     try:
@@ -14,7 +17,9 @@ def _get_workspace() -> Workspace:
 
         return Workspace(SUBSCRIPTION_ID, RESOURCE_GROUP, WORKSPACE_NAME)
 
+
 def get_dataset_as_dataframe(name: str) -> pd.DataFrame:
     workspace = _get_workspace()
     dataset = Dataset.get_by_name(workspace, name)
     return dataset.to_pandas_dataframe()
+
